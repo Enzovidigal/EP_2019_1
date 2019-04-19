97dca454664640b31ab0c2e7a1a5a9cbae05f410
Joao Zsigmond <jzsiggy@gmail.com>
2019-04-19 17:25:20 -0300

    Adicionando 5o feature - cenarios.txt

--- ep1.py
+++ ep1.py
@@ -4,41 +4,13 @@
 # - aluno B: Enzo Vidigal, enzolv@al.insper.edu.br
 # - aluno A: Joao Zsigmond, joaoz@al.insper.edu.br
 import random
+import json
 import sys
 
 def carregar_cenarios():
-    cenarios = {
-        "inicio": {
-            "titulo": "Saguao do perigo",
-            "descricao": "Voce esta no saguao de entrada do insper",
-            "opcoes": {
-                "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
-            }
-        },
-        "andar professor": {
-            "titulo": "Andar do desespero",
-            "descricao": "Voce chegou ao andar da sala do seu professor",
-            "opcoes": {
-                "inicio": "Tomar o elevador para o saguao de entrada",
-                "professor": "Falar com o professor"
-            }
-        },
-        "professor": {
-            "titulo": "O monstro do Python",
-            "descricao": "Voce foi pedir para o professor adiar o EP. "
-                         "O professor revelou que é um monstro disfarçado "
-                         "e devorou sua alma.",
-            "opcoes": {}
-        },
-        "biblioteca": {
-            "titulo": "Caverna da tranquilidade",
-            "descricao": "Voce esta na biblioteca",
-            "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-            }
-        }
-    }
+    with open('cenarios.txt', 'r') as episodios:
+        cenarios_dict = json.load(episodios)
+        cenarios = cenarios_dict
     nome_cenario_atual = "inicio"
     return cenarios, nome_cenario_atual
 
@@ -91,8 +63,8 @@ def main():
                     print("\n\nO MONSTRO TE DERROTOU")
                     game_over = True
                     break
-                elif vida_monstro == 0:
-                    print("\n\n Vc derrotou o monstro, ganhou a chave que abre o próximo nível!")                    
+                elif vida_monstro == 0 and 'Chave1' not in inventario:
+                    print("\n\nVc derrotou o monstro, ganhou um prêmio\nAgora tens a chave para passar para entrar na sala do mago!")                    
                     inventario.append('Chave1')       
                     break
                 
